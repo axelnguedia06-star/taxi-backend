6cfe12985ab1f5bbfac38457e0575a9948a7b30e
axelnguedia06-star <axelnguedia06@gmail.com>
2026-05-15 13:29:27 +0100

    Update server-render-complete.js

--- server-render-complete.js
+++ server-render-complete.js
@@ -163,8 +163,8 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
-  exposedHeaders: ['Content-Length', 'X-Request-Id'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With', 'Cache-Control', 'Pragma'],
+  exposedHeaders: ['Content-Length', 'X-Request-Id', 'Authorization'],
   maxAge: 86400
 };
 app.use(cors(corsOptions));
